7a9e17322a6564a108ca4da3e6c9a8210cf58fda
Daryll Ko <daryll.ko@gmail.com>
2023-04-05 22:38:46 +0800

    feat: add `json` and `yaml` readers

--- src/reads.py
+++ src/reads.py
@@ -1,8 +1,13 @@
 import os
 import xmltodict
+import json
+import yaml
 
 INPUTS_FOLDER = "inputs"
+
 XMP_FOLDER = "xmp"
+JSON_FOLDER = "json"
+YAML_FOLDER = "yaml"
 
 
 def read_xmp(filename: str) -> dict[str, any]:
@@ -13,3 +18,23 @@ def read_xmp(filename: str) -> dict[str, any]:
         "r",
     ) as input_file:
         return xmltodict.parse(input_file.read())["content"]
+
+
+def read_json(filename: str) -> dict[str, any]:
+    with open(
+        os.path.join(
+            os.path.dirname(__file__), INPUTS_FOLDER, JSON_FOLDER, f"{filename}.json"
+        ),
+        "r",
+    ) as input_file:
+        return json.loads(input_file.read())
+
+
+def read_yaml(filename: str) -> dict[str, any]:
+    with open(
+        os.path.join(
+            os.path.dirname(__file__), INPUTS_FOLDER, YAML_FOLDER, f"{filename}.yaml"
+        ),
+        "r",
+    ) as input_file:
+        return yaml.load(input_file.read())
