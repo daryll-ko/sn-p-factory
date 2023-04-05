276e29fa691ef80e32ff45ed4dcd3b62400b2315
Daryll Ko <daryll.ko@gmail.com>
2023-04-05 10:28:02 +0800

    chore: invoke black

--- src/main.py
+++ src/main.py
@@ -3,13 +3,18 @@ import xmltodict
 
 INPUT_FOLDER = "inputs"
 
+
 def parse_xmp(filename: str) -> dict:
-    with open(os.path.join(os.path.dirname(__file__), INPUT_FOLDER, filename), 'r') as input_file:
-        return xmltodict.parse(input_file.read())['content']
+    with open(
+        os.path.join(os.path.dirname(__file__), INPUT_FOLDER, filename), "r"
+    ) as input_file:
+        return xmltodict.parse(input_file.read())["content"]
+
 
 def parse_dict(d: dict):
     for k, _ in d.items():
         print(k)
+
 
 dict = parse_xmp("ex1 - 3k+3 spiker.xmp")
 parse_dict(dict)
