1ea5bca710094f160b4af6967bfacd18ea9526e6
Daryll Ko <daryll.ko@gmail.com>
2023-04-05 10:25:23 +0800

    feat: do basic parsing

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import os
 import xmltodict
-import json
 
 INPUT_FOLDER = "inputs"
 
-def parse_xmp(filename: str):
+def parse_xmp(filename: str) -> dict:
     with open(os.path.join(os.path.dirname(__file__), INPUT_FOLDER, filename), 'r') as input_file:
-        return xmltodict.parse(input_file.read())
+        return xmltodict.parse(input_file.read())['content']
+
+def parse_dict(d: dict):
+    for k, _ in d.items():
+        print(k)
 
 dict = parse_xmp("ex1 - 3k+3 spiker.xmp")
-print(json.dumps(dict, indent=4))
+parse_dict(dict)
