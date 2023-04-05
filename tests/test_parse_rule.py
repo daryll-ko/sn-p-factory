d79b2032af3abbdd9f204814097617c84ddff6aa
Daryll Ko <daryll.ko@gmail.com>
2023-04-05 15:23:23 +0800

    test: add more test cases for `test_parse_rule`

--- tests/test_parse_rule.py
+++ tests/test_parse_rule.py
@@ -6,5 +6,19 @@ import unittest
 
 class TestRuleParser(unittest.TestCase):
     def test_basic(self):
-        expected = Rule("a", 1, 1, 0)
-        self.assertEqual(parse_rule("a/a->a;0"), expected)
+        self.assertEqual(parse_rule("a/a->a;0"), Rule("a", 1, 1, 0))
+
+    def test_zero_produced(self):
+        self.assertEqual(parse_rule("a/a->0;0"), Rule("a", 1, 0, 0))
+
+    def test_multiple_consumed(self):
+        self.assertEqual(parse_rule("a/2a->a;0"), Rule("a", 2, 1, 0))
+
+    def test_multiple_produced(self):
+        self.assertEqual(parse_rule("a/2a->2a;0"), Rule("a", 2, 2, 0))
+
+    def test_complex_regex(self):
+        self.assertEqual(parse_rule("aU(2a)+/a->a;0"), Rule("aU(2a)+", 1, 1, 0))
+
+    def test_nonzero_delay(self):
+        self.assertEqual(parse_rule("a/a->a;1"), Rule("a", 1, 1, 1))
