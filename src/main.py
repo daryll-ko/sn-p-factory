e08914d4d48c04875c639419b1d15453f9bdc625
Daryll Ko <daryll.ko@gmail.com>
2023-04-06 10:21:40 +0800

    feat: convert all initial samples

--- src/main.py
+++ src/main.py
@@ -1,28 +1,39 @@
 from src.reads import read_xmp, read_json, read_yaml
 from src.parsers import parse_xmp_dict, parse_dict
 
+import os
 import json
 import yaml
-import xmltodict
 
 
 def main():
-    filename = "3k+3"
+    inputs_path = os.path.join(os.path.dirname(__file__), "data")
 
-    d_xmp = read_xmp(filename)
-    system_xmp = parse_xmp_dict(d_xmp, filename)
-    print(xmltodict.unparse(system_xmp.to_dict_old(), pretty=True, indent=" " * 4))
-    print()
+    xmp_path = os.path.join(inputs_path, "xmp")
+    json_path = os.path.join(inputs_path, "json")
+    yaml_path = os.path.join(inputs_path, "yaml")
 
-    d_json = read_json(filename)
-    system_json = parse_dict(d_json)
-    print(json.dumps(system_json.to_dict(), indent=4))
-    print()
+    for file in os.listdir(xmp_path):
+        filename = os.path.splitext(file)[0]
 
-    d_yaml = read_yaml(filename)
-    system_yaml = parse_dict(d_yaml)
-    print(yaml.dump(system_yaml.to_dict(), sort_keys=False, indent=2))
-    print()
+        d_1 = read_xmp(filename)
+        system_1 = parse_xmp_dict(d_1, filename)
+
+        with open(os.path.join(json_path, f"{filename}.json"), "w") as json_output_file:
+            json_output_file.write(json.dumps(system_1.to_dict(), indent=4))
+
+        with open(os.path.join(yaml_path, f"{filename}.yaml"), "w") as yaml_output_file:
+            yaml_output_file.write(
+                yaml.dump(system_1.to_dict(), sort_keys=False, indent=2)
+            )
+
+        d_2 = read_json(filename)
+        system_2 = parse_dict(d_2)
+
+        d_3 = read_yaml(filename)
+        system_3 = parse_dict(d_3)
+
+        print(system_1 == system_2 == system_3)
 
 
 if __name__ == "__main__":
