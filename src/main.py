3bdcbc2133a2902ff22e3c2127f6255d3cded289
Daryll Ko <daryll.ko@gmail.com>
2023-04-06 09:54:35 +0800

    feat: update `main`

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from src.reads import read_xmp
-from src.parsers import parse_xmp_dict
+from src.reads import read_xmp, read_json, read_yaml
+from src.parsers import parse_xmp_dict, parse_dict
 
 import json
 import yaml
@@ -8,13 +8,21 @@ import xmltodict
 
 def main():
     filename = "3k+3"
-    d = read_xmp(filename)
-    system = parse_xmp_dict(d, filename)
-    print(json.dumps(system.to_dict(), indent=4))
+
+    d_xmp = read_xmp(filename)
+    system_xmp = parse_xmp_dict(d_xmp, filename)
+    print(xmltodict.unparse(system_xmp.to_dict_old(), pretty=True, indent=" " * 4))
     print()
-    print(xmltodict.unparse(system.to_dict_old(), pretty=True, indent="    "))
+
+    d_json = read_json(filename)
+    system_json = parse_dict(d_json)
+    print(json.dumps(system_json.to_dict(), indent=4))
     print()
-    print(yaml.dump(system.to_dict(), sort_keys=False, indent=2))
+
+    d_yaml = read_yaml(filename)
+    system_yaml = parse_dict(d_yaml)
+    print(yaml.dump(system_yaml.to_dict(), sort_keys=False, indent=2))
+    print()
 
 
 if __name__ == "__main__":
