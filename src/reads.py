35562077aef94cada87c36e5f89c8ad02ddfb09c
Daryll Ko <daryll.ko@gmail.com>
2023-05-02 21:21:59 +0800

    refactor: use path constants instead of name constants

--- src/reads.py
+++ src/reads.py
@@ -3,18 +3,16 @@ import xmltodict
 import json
 import yaml
 
-INPUTS_FOLDER = "data"
+INPUTS_PATH = os.path.join(os.path.dirname(__file__), "data")
 
-XMP_FOLDER = "xmp"
-JSON_FOLDER = "json"
-YAML_FOLDER = "yaml"
+XMP_PATH = os.path.join(INPUTS_PATH, "xmp")
+JSON_PATH = os.path.join(INPUTS_PATH, "json")
+YAML_PATH = os.path.join(INPUTS_PATH, "yaml")
 
 
 def read_xmp(filename: str) -> dict[str, any]:
     with open(
-        os.path.join(
-            os.path.dirname(__file__), INPUTS_FOLDER, XMP_FOLDER, f"{filename}.xmp"
-        ),
+        os.path.join(XMP_PATH, f"{filename}.xmp"),
         "r",
     ) as input_file:
         return xmltodict.parse(input_file.read())["content"]
@@ -22,9 +20,7 @@ def read_xmp(filename: str) -> dict[str, any]:
 
 def read_json(filename: str) -> dict[str, any]:
     with open(
-        os.path.join(
-            os.path.dirname(__file__), INPUTS_FOLDER, JSON_FOLDER, f"{filename}.json"
-        ),
+        os.path.join(JSON_PATH, f"{filename}.json"),
         "r",
     ) as input_file:
         return json.loads(input_file.read())
@@ -32,9 +28,7 @@ def read_json(filename: str) -> dict[str, any]:
 
 def read_yaml(filename: str) -> dict[str, any]:
     with open(
-        os.path.join(
-            os.path.dirname(__file__), INPUTS_FOLDER, YAML_FOLDER, f"{filename}.yaml"
-        ),
+        os.path.join(YAML_PATH, f"{filename}.yaml"),
         "r",
     ) as input_file:
         return yaml.load(input_file.read(), Loader=yaml.Loader)
