f37107533cd451604a09eeb64b22e966a19937b4
Daryll Ko <daryll.ko@gmail.com>
2023-05-04 19:55:44 +0800

    refactor!: remove `from` field of `Synapse`

--- src/classes/Synapse.py
+++ src/classes/Synapse.py
@@ -3,9 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Synapse:
-    start: int
-    end: int
+    to: int
     weight: int
 
     def to_dict(self) -> dict[str, any]:
-        return {"from": self.start, "to": self.end, "weight": self.weight}
+        return vars(self)
