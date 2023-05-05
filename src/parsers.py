15792acd61d808324ec9c411eadde6badbad4257
Daryll Ko <daryll.ko@gmail.com>
2023-05-05 23:12:49 +0800

    fix: use new json/yaml format for updated parser

--- src/parsers.py
+++ src/parsers.py
@@ -19,7 +19,7 @@ def parse_rule_xmp(s: str) -> Rule:
 
 
 def parse_neuron_xmp(
-    d: dict[str, any], to_id: dict[str, int], spike_train: list[int], is_output: bool
+    d: dict[str, any], to_id: dict[str, int], spike_times: list[int], is_output: bool
 ) -> Neuron:
     id = to_id[d["id"]]
     label = d["id"]
@@ -29,7 +29,7 @@ def parse_neuron_xmp(
     rules = list(map(parse_rule_xmp, d["rules"].split())) if "rules" in d else []
     spikes = int(d["spikes"])
     downtime = int(d["delay"]) if "delay" in d else 0
-    is_input = len(spike_train) > 0
+    is_input = len(spike_times) > 0
 
     synapses = []
 
@@ -49,7 +49,7 @@ def parse_neuron_xmp(
         synapses,
         is_input,
         is_output,
-        spike_train,
+        spike_times,
     )
 
 
@@ -65,14 +65,14 @@ def parse_dict_xmp(d: dict[str, any], filename: str) -> System:
             to_id[k] = current_id
             current_id += 1
 
-    input_neurons = dict()
+    input_neurons = {}
     environment_neurons = set()
     output_neurons = set()
 
     for v in d.values():
         if "isInput" in v and v["isInput"] and "outWeights" in v and "bitstring" in v:
             for inner_k in v["outWeights"].keys():
-                input_neurons[to_id[inner_k]] = Neuron.compress_to_spike_train(
+                input_neurons[to_id[inner_k]] = Neuron.compress_to_spike_times(
                     v["bitstring"]
                 )
         if "isOutput" in v and v["isOutput"]:
@@ -86,9 +86,9 @@ def parse_dict_xmp(d: dict[str, any], filename: str) -> System:
 
     filtered_dicts = list(
         filter(
-            lambda dict: not (
-                ("isInput" in dict and dict["isInput"])
-                or ("isOutput" in dict and dict["isOutput"])
+            lambda inner_d: not (
+                ("isInput" in inner_d and inner_d["isInput"] == "true")
+                or ("isOutput" in inner_d and inner_d["isOutput"] == "true")
             ),
             d.values(),
         )
@@ -98,7 +98,7 @@ def parse_dict_xmp(d: dict[str, any], filename: str) -> System:
         parse_neuron_xmp(
             v,
             to_id,
-            input_neurons[to_id[v["id"]]] if to_id(v["id"]) in input_neurons else [],
+            input_neurons[to_id[v["id"]]] if to_id[v["id"]] in input_neurons else [],
             to_id[v["id"]] in output_neurons,
         )
         for v in filtered_dicts
@@ -130,16 +130,30 @@ def parse_neuron(d: dict[str, any]) -> Neuron:
     rules = [parse_rule(rule) for rule in d["rules"]]
     spikes = int(d["spikes"])
     downtime = int(d["downtime"])
+    synapses = [parse_synapse(synapse) for synapse in d["synapses"]]
+    is_input = bool(d["isInput"])
+    is_output = bool(d["isOutput"])
+    spike_times = list(map(int, d["spikeTimes"]))
 
-    return Neuron(id, label, position, rules, spikes, downtime)
+    return Neuron(
+        id,
+        label,
+        position,
+        rules,
+        spikes,
+        downtime,
+        synapses,
+        is_input,
+        is_output,
+        spike_times,
+    )
 
 
 def parse_synapse(d: dict[str, any]) -> Neuron:
-    start = int(d["from"])
-    end = int(d["to"])
+    to = int(d["to"])
     weight = int(d["weight"])
 
-    return Synapse(start, end, weight)
+    return Synapse(to, weight)
 
 
 def parse_dict(d: dict[str, any]) -> System:
