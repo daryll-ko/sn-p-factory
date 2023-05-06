ae5616014c6a0c7a8074909e92269bc453d18404
Daryll Ko <daryll.ko@gmail.com>
2023-05-06 11:14:51 +0800

    feat: optimize simulation algorithm

--- src/classes/System.py
+++ src/classes/System.py
@@ -1,6 +1,7 @@
 import re
 import random
 
+from heapq import heappush, heappop
 from collections import defaultdict
 from dataclasses import dataclass
 from .Neuron import Neuron
@@ -64,12 +65,7 @@ class System:
 
         return {"content": dict(neuron_entries)}
 
-    def simulate_one_step(self, time: int) -> bool:
-        print()
-        print(f"Time: {time}")
-        print()
-        print(f"System: {self}")
-
+    def simulate(self) -> bool:
         to_index = defaultdict(int)
         current_index = 0
 
@@ -78,34 +74,64 @@ class System:
                 to_index[neuron.id] = current_index
                 current_index += 1
 
-        N = current_index
-        net_gain = [0 for _ in range(N)]
+        incoming_spikes = [[] for _ in range(current_index)]  # @start of timestep
+
+        time = 0
+        done = False
 
         for neuron in self.neurons:
             if neuron.is_input:
-                if time in neuron.spike_times:
-                    neuron.spikes += 1
-            i = to_index[neuron.id]
-            possible_indices = []
-            for index, rule in enumerate(neuron.rules):
-                python_regex = Rule.get_python_regex(rule.regex)
-                result = re.match(python_regex, "a" * neuron.spikes)
-                if result:
-                    possible_indices.append(index)
-            if len(possible_indices) > 0:
-                chosen_index = random.choice(possible_indices)
-                rule = neuron.rules[chosen_index]
-                net_gain[i] -= rule.consumed
-                for synapse in neuron.synapses:
-                    to, w = synapse.to, synapse.weight
-                    net_gain[to_index[to]] += rule.produced * w
-                if neuron.is_output:
-                    neuron.spike_times.append(time)
-                neuron.downtime = rule.delay
+                for t in neuron.spike_times:
+                    heappush(incoming_spikes[to_index[neuron.id]], (t, 1))
 
-        for neuron in self.neurons:
-            if neuron.downtime == 0:
-                neuron.spikes += net_gain[to_index[neuron.id]]
-            neuron.downtime = max(neuron.downtime - 1, 0)
+        while not done and time <= 2 * 10**1:
+            for neuron in self.neurons:
+                heap = incoming_spikes[to_index[neuron.id]]
+                if neuron.downtime == 0:
+                    while len(heap) > 0 and heap[0][0] == time:
+                        neuron.spikes += heap[0][1]
+                        heappop(heap)
+                neuron.downtime = max(neuron.downtime - 1, 0)
 
-        return any(value != 0 for value in net_gain)
+            print()
+            print(f"System (t = {time}):")
+            print(self)
+
+            for neuron in self.neurons:
+                if neuron.downtime == 0:
+                    possible_indices = []
+
+                    for index, rule in enumerate(neuron.rules):
+                        python_regex = Rule.get_python_regex(rule.regex)
+                        result = re.match(python_regex, "a" * neuron.spikes)
+                        if result:
+                            possible_indices.append(index)
+
+                    if len(possible_indices) > 0:
+                        chosen_index = random.choice(possible_indices)
+                        rule = neuron.rules[chosen_index]
+                        neuron.spikes -= rule.consumed
+                        for synapse in neuron.synapses:
+                            to, weight = synapse.to, synapse.weight
+                            heappush(
+                                incoming_spikes[to_index[to]],
+                                (time + rule.delay + 1, rule.produced * weight),
+                            )
+                        if neuron.is_output:
+                            print(f"{neuron.label} -> env")
+                            print()
+                            neuron.spike_times.append(time + rule.delay)
+                        neuron.downtime = rule.delay
+
+            print("- " * 5 + "-")
+
+            done = all([len(heap) == 0 for heap in incoming_spikes])
+            time += 1
+
+    def __str__(self) -> str:
+        nl = "\n"
+        return f"""
+{nl.join(list(map(
+    lambda neuron: f"{neuron.label}: {neuron.spikes}/{neuron.downtime}", self.neurons
+)))}
+"""
