dd4ec045f3f8844cb047d2818d4d1fa214c58293
Daryll Ko <daryll.ko@gmail.com>
2023-05-06 12:10:29 +0800

    test: add test cases with no braces

--- tests/test_json_to_python_regex.py
+++ tests/test_json_to_python_regex.py
@@ -13,11 +13,20 @@ class TestJsonToPythonRegex(unittest.TestCase):
     def test_exponent(self):
         self.assertEqual(Rule.json_to_python_regex(r"a^{2}"), r"a{2}")
 
+    def test_exponent_no_braces(self):
+        self.assertEqual(Rule.json_to_python_regex(r"a^2"), r"a{2}")
+
     def test_plus(self):
         self.assertEqual(Rule.json_to_python_regex(r"a^{+}"), r"a+")
 
+    def test_plus_no_braces(self):
+        self.assertEqual(Rule.json_to_python_regex(r"a^+"), r"a+")
+
     def test_star(self):
         self.assertEqual(Rule.json_to_python_regex(r"a^{*}"), r"a*")
+
+    def test_star_no_braces(self):
+        self.assertEqual(Rule.json_to_python_regex(r"a^*"), r"a*")
 
     def test_union(self):
         self.assertEqual(Rule.json_to_python_regex(r"a \cup a"), r"a|a")
@@ -26,4 +35,9 @@ class TestJsonToPythonRegex(unittest.TestCase):
         self.assertEqual(
             Rule.json_to_python_regex(r"(a^{2} \cup a^{3})^{+} \cup a^{*}"),
             r"(a{2}|a{3})+|a*",
+        )
+
+    def test_complex_no_braces(self):
+        self.assertEqual(
+            Rule.json_to_python_regex(r"(a^2 \cup a^3)^+ \cup a^*"), r"(a{2}|a{3})+|a*"
         )
