1525f163be22973bb23a40ab9c418c3408488800
Daryll Ko <daryll.ko@gmail.com>
2023-05-06 12:59:32 +0800

    test: add test cases for `python_to_xmp_regex`

---
+++ tests/test_python_to_xmp_regex.py
@@ -0,0 +1,17 @@
+import unittest
+
+from src.classes.Rule import Rule
+
+
+class TestPythonToXmpRegex(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(Rule.python_to_xmp_regex(r"a"), r"a")
+
+    def test_greater_than_one(self):
+        self.assertEqual(Rule.python_to_xmp_regex(r"a{3}"), r"3a")
+
+    def test_one_with_star(self):
+        self.assertEqual(Rule.python_to_xmp_regex(r"(a)*"), r"(a)*")
+
+    def test_greater_than_one_with_star(self):
+        self.assertEqual(Rule.python_to_xmp_regex(r"(a{2})*"), r"(2a)*")
