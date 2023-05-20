400409cc450fd9602e956ab5256219ddc8ff859f
Daryll Ko <daryll.ko@gmail.com>
2023-05-20 21:08:29 +0800

    refactor: generalize writes

--- src/writes.py
+++ src/writes.py
@@ -1,52 +1,17 @@
-from src.globals import XMP_PATH, JSON_PATH, YAML_PATH
+from src.globals import FORMATS
 
 import os
-import xmltodict
-import json
-import yaml
 
 
-def write_xmp(d: dict[str, any], filename: str, simulating: bool):
+def write(d: dict[str, any], filename: str, format: str, simulating: bool) -> None:
+    format_data = FORMATS[format]
     if simulating:
-        directory_path = os.path.join(XMP_PATH, "|".join(filename.split("|")[:-1]))
+        directory_path = os.path.join(
+            format_data.path, "|".join(filename.split("|")[:-1])
+        )
         if not os.path.exists(directory_path):
             os.mkdir(directory_path)
         with open(
-            os.path.join(directory_path, f"{filename}.xmp"), "w"
-        ) as xmp_output_file:
-            xmp_output_file.write(
-                xmltodict.unparse(d, pretty=True, newl="\n", indent=" " * 4)
-            )
-    else:
-        with open(os.path.join(XMP_PATH, f"{filename}.xmp"), "w") as xmp_output_file:
-            xmp_output_file.write(
-                xmltodict.unparse(d, pretty=True, newl="\n", indent=" " * 4)
-            )
-
-
-def write_json(d: dict[str, any], filename: str, simulating: bool):
-    if simulating:
-        directory_path = os.path.join(JSON_PATH, "|".join(filename.split("|")[:-1]))
-        if not os.path.exists(directory_path):
-            os.mkdir(directory_path)
-        with open(
-            os.path.join(directory_path, f"{filename}.json"), "w"
-        ) as json_output_file:
-            json_output_file.write(json.dumps(d, indent=2))
-    else:
-        with open(os.path.join(JSON_PATH, f"{filename}.json"), "w") as json_output_file:
-            json_output_file.write(json.dumps(d, indent=2))
-
-
-def write_yaml(d: dict[str, any], filename: str, simulating: bool):
-    if simulating:
-        directory_path = os.path.join(YAML_PATH, "|".join(filename.split("|")[:-1]))
-        if not os.path.exists(directory_path):
-            os.mkdir(directory_path)
-        with open(
-            os.path.join(directory_path, f"{filename}.yaml"), "w"
-        ) as yaml_output_file:
-            yaml_output_file.write(yaml.dump(d, sort_keys=False, indent=2))
-    else:
-        with open(os.path.join(YAML_PATH, f"{filename}.yaml"), "w") as yaml_output_file:
-            yaml_output_file.write(yaml.dump(d, sort_keys=False, indent=2))
+            os.path.join(directory_path, f"{filename}.{format_data.extension}"), "w"
+        ) as output_file:
+            output_file.write(format_data.write_function(d))
