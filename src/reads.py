0c307fc4f7d3c7222904d1a960e23ed90f2f6ee4
Daryll Ko <daryll.ko@gmail.com>
2023-05-20 21:16:53 +0800

    refactor: generalize reads

--- src/reads.py
+++ src/reads.py
@@ -1,30 +1,11 @@
-from src.globals import XMP_PATH, JSON_PATH, YAML_PATH
+from src.globals import FORMATS
 
 import os
-import xmltodict
-import json
-import yaml
 
 
-def read_xmp(filename: str) -> dict[str, any]:
+def read(filename: str, format: str) -> dict[str, any]:
+    format_data = FORMATS[format]
     with open(
-        os.path.join(XMP_PATH, f"{filename}.xmp"),
-        "r",
+        os.path.join(format_data.path, f"{filename}.{format_data.extension}"), "r"
     ) as input_file:
-        return xmltodict.parse(input_file.read())["content"]
-
-
-def read_json(filename: str) -> dict[str, any]:
-    with open(
-        os.path.join(JSON_PATH, f"{filename}.json"),
-        "r",
-    ) as input_file:
-        return json.loads(input_file.read())
-
-
-def read_yaml(filename: str) -> dict[str, any]:
-    with open(
-        os.path.join(YAML_PATH, f"{filename}.yaml"),
-        "r",
-    ) as input_file:
-        return yaml.load(input_file.read(), Loader=yaml.Loader)
+        return format_data.read_function(input_file.read())
