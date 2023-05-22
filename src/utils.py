133961a98cda1ac90e86824f74ad37d17becdd0e
Daryll Ko <daryll.ko@gmail.com>
2023-05-22 15:42:06 +0800

    refactor: merge `read` and `write` into one file

--- src/utils.py
+++ src/utils.py
@@ -17,3 +17,18 @@ def read(filename: str, format: Format, simulating: bool) -> dict[str, Any]:
         "r",
     ) as input_file:
         return format.read_function(input_file.read())
+
+
+def write(d: dict[str, Any], filename: str, format: Format, simulating: bool) -> None:
+    if simulating:
+        directory_path = os.path.join(format.path, filename.split("[")[0])
+        if not os.path.exists(directory_path):
+            os.mkdir(directory_path)
+    with open(
+        os.path.join(
+            directory_path if simulating else format.path,
+            f"{filename}.{format.extension}",
+        ),
+        "w",
+    ) as output_file:
+        output_file.write(format.write_function(d))
