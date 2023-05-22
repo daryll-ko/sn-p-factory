e2f8ccb42fbf867043bb20102a9911c965a8ea78
Daryll Ko <daryll.ko@gmail.com>
2023-05-22 11:13:54 +0800

    feat: add `simulating` field to `read`

--- src/reads.py
+++ src/reads.py
@@ -4,8 +4,16 @@ from src.classes.Format import Format
 import os
 
 
-def read(filename: str, format: Format) -> dict[str, Any]:
+def read(filename: str, format: Format, simulating: bool) -> dict[str, Any]:
+    if simulating:
+        directory_path = os.path.join(format.path, filename.split("[")[0])
+        if not os.path.exists(directory_path):
+            os.mkdir(directory_path)
     with open(
-        os.path.join(format.path, f"{filename}.{format.extension}"), "r"
+        os.path.join(
+            directory_path if simulating else format.path,
+            f"{filename}.{format.extension}",
+        ),
+        "w",
     ) as input_file:
         return format.read_function(input_file.read())
