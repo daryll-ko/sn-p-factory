8a1e9567966323640614dcc04734f13cb840f703
Daryll Ko <daryll.ko@gmail.com>
2023-05-22 10:45:46 +0800

    refactor: pass str instead of `TestName` to `write` for now

--- src/writes.py
+++ src/writes.py
@@ -1,16 +1,12 @@
 from typing import Any
 from src.classes.Format import Format
-from src.classes.TestName import TestName
 
 import os
 
 
-def write(
-    d: dict[str, Any], testname: TestName, format: Format, simulating: bool
-) -> None:
-    filename = testname.make_filename()
+def write(d: dict[str, Any], filename: str, format: Format, simulating: bool) -> None:
     if simulating:
-        directory_path = os.path.join(format.path, testname.name)
+        directory_path = os.path.join(format.path, filename.split("[")[0])
         if not os.path.exists(directory_path):
             os.mkdir(directory_path)
     with open(
