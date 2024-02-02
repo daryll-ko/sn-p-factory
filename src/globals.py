08a9d6fd04fbd9a943007355906cbdecb29b767a
Daryll Ko <daryll.ko@gmail.com>
2024-02-02 22:40:23 +0800

    update: `DATA_PATH` -> `TEST_CASE_FOLDER`, remove `path` attrs

--- src/globals.py
+++ src/globals.py
@@ -5,34 +5,28 @@ import yaml
 
 from src.classes.Format import Format
 
-DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
+TEST_CASE_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
 
 XML = Format(
-    path=os.path.join(DATA_PATH, "xml"),
     extension="xml",
-    read_function=lambda s: xmltodict.parse(s)["content"],
-    write_function=lambda d: xmltodict.unparse(
-        d, pretty=True, newl="\n", indent=" " * 4
-    ),
+    str_to_dict=lambda s: xmltodict.parse(s)["content"],
+    dict_to_str=lambda d: xmltodict.unparse(d, pretty=True, newl="\n", indent=" " * 4),
 )
 
 JSON = Format(
-    path=os.path.join(DATA_PATH, "json"),
     extension="json",
-    read_function=lambda s: json.loads(s),
-    write_function=lambda d: json.dumps(d, indent=2),
+    str_to_dict=lambda s: json.loads(s),
+    dict_to_str=lambda d: json.dumps(d, indent=2),
 )
 
 YAML = Format(
-    path=os.path.join(DATA_PATH, "yaml"),
     extension="yaml",
-    read_function=lambda s: yaml.load(s, Loader=yaml.Loader),
-    write_function=lambda d: yaml.dump(d, sort_keys=False, indent=2),
+    str_to_dict=lambda s: yaml.load(s, Loader=yaml.Loader),
+    dict_to_str=lambda d: yaml.dump(d, sort_keys=False, indent=2),
 )
 
 LOG = Format(
-    path=os.path.join(DATA_PATH, "log"),
     extension="log",
-    read_function=lambda _: {},
-    write_function=lambda _: "",
+    str_to_dict=lambda _: {},
+    dict_to_str=lambda _: "",
 )
