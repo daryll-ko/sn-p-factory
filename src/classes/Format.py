71fc5fc3a4f188e7e08d2160d045bbff989111f6
Daryll Ko <daryll.ko@gmail.com>
2024-02-02 22:39:33 +0800

    update: make `path` attr of `Format` a getter based on `extension`

--- src/classes/Format.py
+++ src/classes/Format.py
@@ -1,10 +1,16 @@
-from typing import Callable
+import os
+
 from dataclasses import dataclass
+from typing import Callable
+
+from src.globals import TEST_CASE_FOLDER
 
 
 @dataclass
 class Format:
-    path: str
     extension: str
     str_to_dict: Callable[[str], dict]
     dict_to_str: Callable[[dict], str]
+
+    def get_path(self):
+        return os.path.join(TEST_CASE_FOLDER, self.extension)
