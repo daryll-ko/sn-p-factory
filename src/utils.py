debf6ceb95cac227dcfb74d9e1b6f6a091a316f7
Daryll Ko <daryll.ko@gmail.com>
2024-02-02 22:27:46 +0800

    docs: add docstrings to reader and writer

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,10 @@ import os
 
 
 def read_dict(filename: str, format: Format) -> dict:
+    """
+    Reads the file with name `{filename}.{format extension}`
+    and returns a dict that contains the SN P system's info.
+    """
     with open(
         os.path.join(format.path, f"{filename}.{format.extension}"), "r"
     ) as input_file:
@@ -11,6 +15,10 @@ def read_dict(filename: str, format: Format) -> dict:
 
 
 def write_dict(d: dict, filename: str, format: Format) -> None:
+    """
+    Takes in a dict `d` and writes its contents to the file
+    with name `{filename}.{format extension}` via `format`.
+    """
     with open(
         os.path.join(format.path, f"{filename}.{format.extension}"), "w"
     ) as output_file:
