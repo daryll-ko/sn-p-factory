d0b7ffbefeb48410872b0c9029c60fd43d854ba4
Daryll Ko <daryll.ko@gmail.com>
2025-02-10 21:59:59 +0800

    refactor: implement classes for file formats

---
+++ classes/FileFormat.py
@@ -0,0 +1,46 @@
+import json
+import xmltodict
+import yaml
+from utils.logging import tred
+from typing import Protocol
+
+
+class FileFormat(Protocol):
+    def str_to_dict(self, s: str) -> dict: ...
+    def dict_to_str(self, d: dict) -> str: ...
+
+
+class XML:
+    def str_to_dict(self, s: str) -> dict:
+        return xmltodict.parse(s)["content"]
+
+    def dict_to_str(self, d: dict) -> str:
+        return xmltodict.unparse(d, pretty=True, newl="\n", indent=" " * 4)
+
+
+class JSON:
+    def str_to_dict(self, s: str) -> dict:
+        return json.loads(s)
+
+    def dict_to_str(self, d: dict) -> str:
+        return json.dumps(d, indent=2)
+
+
+class YAML:
+    def str_to_dict(self, s: str) -> dict:
+        return yaml.load(s, Loader=yaml.Loader)
+
+    def dict_to_str(self, d: dict) -> str:
+        return yaml.dump(d, sort_keys=False, indent=2)
+
+
+def str_to_format(s: str) -> FileFormat:
+    match s:
+        case "xml":
+            return XML()
+        case "json":
+            return JSON()
+        case "yaml":
+            return YAML()
+        case _:
+            raise Exception(tred(":("))
