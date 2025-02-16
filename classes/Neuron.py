c55f2479cbc56c482341d26d58b75553850f8978
Daryll Ko <daryll.ko@gmail.com>
2025-02-16 21:27:10 +0800

    feat: `add` and `remove` ops for `Neuron`

--- classes/Neuron.py
+++ classes/Neuron.py
@@ -13,14 +13,35 @@ class Neuron:
     rules: list[Rule]
     content: Union[int, list[int]]
 
+    def add(self, val: int) -> None:
+        match self.type_:
+            case "regular":
+                assert isinstance(self.content, int)
+                self.content += val
+            case "output":
+                assert isinstance(self.content, list)
+                self.content.append(val)
+            case "input":
+                assert False
+
+    def remove(self, val: int) -> None:
+        match self.type_:
+            case "regular":
+                assert isinstance(self.content, int)
+                self.content -= val
+            case _:
+                assert False
+
     def to_dict(self) -> dict[str, Any]:
         d = {
             "id": self.id,
             "type": self.type_,
             "position": self.position.to_dict(),
-            "content": self.content
-            if isinstance(self.content, int)
-            else "".join(map(str, self.content)),
+            "content": (
+                self.content
+                if isinstance(self.content, int)
+                else "".join(map(str, self.content))
+            ),
         }
         if self.type_ == "regular":
             d["rules"] = [rule.stringify(in_xml=False) for rule in self.rules]
